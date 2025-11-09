de5e797376fd14e81cdcc828706d6cd257a37e55
Ivan Lartsov <lartsov@gmail.com>
2025-11-09 22:42:08 +0300

    fix: console.log

--- node/testHttpServer.js
+++ node/testHttpServer.js
@@ -3,9 +3,9 @@ import http from "node:http";
 const port = Number(process.argv[2]);
 
 http.createServer((req, res) => {
-	res.writesHead(200, { "Content-Type": "text/plain" });
+	res.writeHead(200, { "Content-Type": "text/plain" });
 	res.end(req.socket.remoteAddress);
 })
-	.listen(port, () => {
-		log(`testHttpServer started on http://localhost:${port}`);
+	.listen(port, "127.0.0.1", () => {
+		console.log(`testHttpServer started on http://localhost:${port}`);
 	});
--- node/testTcpServer.js
+++ node/testTcpServer.js
@@ -7,6 +7,6 @@ net.createServer()
 		socket.write(socket.remoteAddress);
 		socket.end();
 	})
-	.listen(port, () => {
-		log(`testTcpServer started on port ${port}`);
+	.listen(port, "127.0.0.1", () => {
+		console.log(`testTcpServer started on port ${port}`);
 	});
